bab1c6af98573dc895320cc237c6622c69caa32f
utshav2008 <utshav2008@gmail.com>
2020-06-15 13:55:14 +0900

    init

--- train.sh
+++ train.sh
@@ -1,15 +1,12 @@
 useradd -d /home/cephuser -m cephuser
 echo linux | passwd cephuser --stdin
-echo &quot;cephuser ALL = (root) NOPASSWD:ALL&quot; | sudo tee
-/etc/sudoers.d/cephuser
+echo "cephuser ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/cephuser
 chmod 0440 /etc/sudoers.d/cephuser
-sed -i s&#39;/Defaults requiretty/#Defaults requiretty&#39;/g
-/etc/sudoers
 systemctl enable --now chronyd
 chronyc sources
-sed -i &#39;s/enforcing/disabled/g&#39; /etc/selinux/config
+sed -i 's/enforcing/disabled/g' /etc/selinux/config
 setenforce 0
-cat &gt; /etc/hosts &lt;&lt;EOF
+cat > /etc/hosts << EOF
 192.168.100.11 admin.example.com admin
 192.168.100.21 node1.example.com node1
 192.168.100.22 node2.example.com node2
