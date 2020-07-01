e2a9613f018e866f19deae691abb09b9ba359ea1
utshav2008 <utshav2008@gmail.com>
2020-07-01 15:06:55 +0900

    LAB3

---
+++ cephadm3.sh
@@ -0,0 +1,8 @@
+# ceph status 
+# ceph mgr module ls | grep enabled_modules -A 6
+# ceph mgr services
+#https://192.168.100.51:8443
+OR using FQDN
+#https://ceph-mon1.example.com:8443
+# ceph dashboard ac-user-set-password admin linux123 --force-password
+# ceph dashboard ac-user-create dashuser dashpass@123 administrator
